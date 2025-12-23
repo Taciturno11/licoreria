db09fd1b45169d33eacc55bad7bf6844b66322c1
mnauca <mnauca@dmsolutions.pe>
2025-12-22 19:37:17 -0500

    a2

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -4,9 +4,18 @@ function LoginPage() {
             <h1 className="text-4xl font-bold text-blue-600 text-center border-b-2 border-blue-600 pb-5">Login</h1>
             <form className="mt-4 flex flex-col gap-3 max-w-sm mx-auto border p-3 shadow-lg">
                 <label >Usuario </label>
-                <input type="text" id="nombre" placeholder="Ingrese su usuario" ></input>
+                <input
+                 type="text" 
+                 id="nombre"
+                 placeholder="Ingrese su usuario" 
+                 className = "border p-2 rounded"
+                 />
                 <label>Contraseña</label>
-                <input type="password" placeholder="Ingrese la contraseña" />
+                <input
+                type="password" 
+                placeholder="Ingrese la contraseña" 
+                className = "border p-2 rounded"
+                />
 
                 <button className="bg-blue-500 text-white p-2 rounded">
                     Entrar
